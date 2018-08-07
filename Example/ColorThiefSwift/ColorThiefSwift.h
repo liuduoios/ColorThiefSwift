7e8157a715725e31eb9ee5060a06820bdaa15f21
liuduo <liuduome@gmail.com>
2018-08-07 10:30:53 +0800

    support Carthage

---
+++ Example/ColorThiefSwift/ColorThiefSwift.h
@@ -0,0 +1,19 @@
+//
+//  ColorThiefSwift.h
+//  ColorThiefSwift
+//
+//  Created by 刘铎 on 2018/8/7.
+//  Copyright © 2018年 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ColorThiefSwift.
+FOUNDATION_EXPORT double ColorThiefSwiftVersionNumber;
+
+//! Project version string for ColorThiefSwift.
+FOUNDATION_EXPORT const unsigned char ColorThiefSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ColorThiefSwift/PublicHeader.h>
+
+
